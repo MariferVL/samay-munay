280fca701cda946499fd51a284c07e005b927d90
MariferVL <mfvl.1186@gmail.com>
2024-11-01 23:57:56 -0300

    Add firebase files and settings

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -22,3 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app); // Initialize Firebase Authentication and get a reference to the service
+const db = getFirestore(app); // Initialize Firestore and get a reference to the service
+
+export { auth, db };
