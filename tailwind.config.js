0a003bc351ef63da8a01559aac561141f90a38ca
MariferVL <mfvl.1186@gmail.com>
2024-11-02 00:41:42 -0300

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'verde-suave': '#A3C9A8',
+        'ocre-claro': '#D3B88C',
+        'azul-pastel': '#C2D6E1',
+        'lila-claro': '#D8C1D1',
+        'blanco-crema': '#FAF5E6',
+        'gris-suave': '#B3B3B3',
+      },
+      fontFamily: {
+        'merriweather': ['Merriweather', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
       },
     },
   },
